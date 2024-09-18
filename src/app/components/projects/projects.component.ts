6113067ad53b76d545178505ccffac991136ce16
Sebastian Reinhold <sebastian.reinhold@yahoo.de>
2024-09-18 16:31:50 +0200

    Projects done

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,11 @@
 import { Component, model } from '@angular/core';
 import { Project } from '../../models/project.model';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
@@ -14,18 +14,27 @@ export class ProjectsComponent {
     window.open(url, '_blank')
   }
 
+  public checkTechnologiesArray(project: Project) {
+    for (var i=0; i<project.technologies.length; i++) {
+      if (project.technologies[i] === "") return false;
+    }
+    return true;
+  }
+
   projects: Project[] = [
     new Project(
       'Youtube2Spotify Converter',
       'Youtube2Spotify is a simple playlist converter to convert your Youtube playlist into a spotify playlist. This app was used to get some practice in python and one of its GUI-tools; in this case CustomTinker.',
       'https://github.com/hardcoreseb/youtube2spotify',
-      '../../../assets/youtube2spotify.webp'
+      '../../../assets/youtube2spotify.webp',
+      ['Python', 'Spotify API', 'Youtube API']
     ),
     new Project(
       'Image to WebP Converter',
       'WIP',
       'WIP',
-      'WIP'
+      'WIP',
+      ['']
     )
   ]
 
--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -3,11 +3,13 @@ export class Project {
     description: string;
     url: string;
     image: string;
+    technologies: string[];
   
-    constructor(title: string, description: string, url: string, image: string) {
+    constructor(title: string, description: string, url: string, image: string, technologies: string[]) {
       this.title = title;
       this.description = description;
       this.url = url;
       this.image = image;
+      this.technologies = technologies;
     }
   }

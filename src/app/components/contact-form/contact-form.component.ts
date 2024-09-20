6682c6dc1bfb632e562dd9a735b56f5197089512
Sebastian Reinhold <sebastian.reinhold@yahoo.de>
2024-09-20 19:26:51 +0200

    Finishing style touches

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
-import { FormBuilder, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormsModule, FormControl, Validators } from '@angular/forms';
+import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
@@ -14,27 +14,42 @@ export class ContactFormComponent {
 
   form: FormGroup = this.fb.group({
     to_name: 'Sebastian',
-    from_name: '',
-    from_email: '',
-    subject: '',
-    message: ''
-  });
+    from_name: ['', Validators.required],
+    from_email: ['', Validators.required, Validators.email],
+    subject: ['', Validators.required],
+    message: ['', Validators.required]
+  }
+);
 
   constructor(private fb: FormBuilder) {}
 
   public async send(){
-    emailjs.init('D_SvhZB3N9x9YkzeK')
-    let response = await emailjs.send("service_qa2m5e7","template_a1sy9dz",{
-      to_name: this.form.value.to_name,
-      from_name: this.form.value.from_name,
-      from_email: this.form.value.from_email,
-      subject: this.form.value.subject,
-      message: this.form.value.message,
-    });
+    if (this.form.valid) {
 
-    alert('Message sent successfully!')
+      emailjs.init('D_SvhZB3N9x9YkzeK')
+      let response = await emailjs.send("service_qa2m5e7","template_a1sy9dz",{
+        to_name: this.form.value.to_name,
+        from_name: this.form.value.from_name,
+        from_email: this.form.value.from_email,
+        subject: this.form.value.subject,
+        message: this.form.value.message,
+      });
+      
+      alert('Message sent successfully!')
+      
+      this.form.reset();
+    }
+    else {
+      alert('Please fill in all required fields correctly.')
 
-    this.form.reset();
+      this.form.markAllAsTouched();
+    }
+  }
+
+  // Method to check if a form field has an error
+  public hasError(field: string, error: string): boolean | null {
+    const control = this.form.get(field);
+    return control && control.hasError(error) && control.touched;
   }
 
 }

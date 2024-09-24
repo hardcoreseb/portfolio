9fa251c0ad25243f35c77d1a73def13f9d401746
Sebastian Reinhold <sebastian.reinhold@yahoo.de>
2024-09-24 16:50:13 +0200

    Navbar responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  collapsed: boolean = true;
+
+  public openNav() {
+    this.collapsed = !this.collapsed;
+  }
 
 }
